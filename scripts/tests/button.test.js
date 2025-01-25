1b2cd971c419238bfff932a536b1406004f1101a
Robert Ellis <mr.bob.ellis@gmail.com>
2025-01-25 10:34:17 +0000

    h1 test

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -3,13 +3,20 @@
  */
 const buttonClick = require("../button");
 
-beforeAll(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+beforeEach(() => {
+    let fs = require("fs");
+    let fileContent = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContent);
+    document.close();
 });
 
 describe("DOM tests", () => {
     test("expects p content to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    });
+    test("H1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
